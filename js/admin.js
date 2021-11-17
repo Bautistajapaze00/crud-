492d737419a0f117cf0f33c2d64988d7786d715f
Emilse <emilsearias@gmail.com>
2021-11-17 17:40:46 -0300

    agregamos el create de un producto

--- js/admin.js
+++ js/admin.js
@@ -1,4 +1,5 @@
 import {campoRequerido, validarNumeros, validarURL, validarGeneral} from './validaciones.js'
+import {Producto} from './productoClass.js';
 
 //traigo el elemento que necesito del html
 let campoCodigo = document.querySelector('#codigo');
@@ -7,6 +8,9 @@ let campoDescripcion = document.querySelector('#descripcion');
 let campoCantidad =  document.querySelector('#cantidad');
 let campoURL =  document.querySelector('#url');
 let formularioProducto = document.querySelector('#formProducto');
+let listaProductos = [];
+
+
 //asociar un evento a un elemento del html
 campoCodigo.addEventListener('blur', ()=>{campoRequerido(campoCodigo)});
 campoProducto.addEventListener('blur', ()=>{campoRequerido(campoProducto)});
@@ -17,7 +21,7 @@ formularioProducto.addEventListener('submit', guardarProducto );
 
 function guardarProducto(e){
     // verificar que todos los datos sean validados
-    e.preventDefault()
+    e.preventDefault();
     if(validarGeneral(campoCodigo,campoProducto, campoDescripcion, campoCantidad, campoURL)){
         //crear un producto
         crearProducto();
@@ -25,7 +29,20 @@ function guardarProducto(e){
 }
 
 function crearProducto(){
-    // crear un producto
-    console.log('áqui tengo que crear el producto')
-    //  new
+    // crear un objeto producto
+    let productoNuevo = new Producto(campoCodigo.value, campoProducto.value, campoDescripcion.value, campoCantidad.value, campoURL.value);
+    // guarda el objeto dentro del arreglo de producto
+    listaProductos.push(productoNuevo);
+    console.log(listaProductos);
+    //limpiar el formulario
+    limpiarFormulario();
+}
+
+function limpiarFormulario(){
+    //limpiamos los value de un formulario
+    formularioProducto.reset();
+    // resetear las clases
+    campoCodigo.className = 'form-control';
+    campoProducto.className = 'form-control';
+    // Tarea modificar todos los className del formulario
 }
